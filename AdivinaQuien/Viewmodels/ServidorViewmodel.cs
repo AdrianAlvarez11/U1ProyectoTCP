4004405c9d1ced1ed79ea2175fdff3e0eed4e478
Adrian Alvarez <adrianalvarezdev@gmail.com>
2026-03-13 00:52:32 -0600

    hacen conexion y navegan, falta programar la vista de seleccionar pokemon

--- AdivinaQuien/Viewmodels/ServidorViewmodel.cs
+++ AdivinaQuien/Viewmodels/ServidorViewmodel.cs
@@ -1,10 +1,13 @@
 ﻿using AdivinaQuienServidor.Models;
+using AdivinaQuienServidor.Services;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace AdivinaQuienServidor.Viewmodels
@@ -20,7 +23,6 @@ namespace AdivinaQuienServidor.Viewmodels
     }
     public class ServidorViewmodel : INotifyPropertyChanged
     {
-      
         public ObservableCollection<Pokemon> Pokemons { get; set; } = new ObservableCollection<Pokemon>()
         {
             new Pokemon { Nombre = "Alakazam" },
@@ -49,14 +51,68 @@ namespace AdivinaQuienServidor.Viewmodels
             new Pokemon { Nombre = "Umbreon" }
         };
 
-        Dispatcher dispatcher;
+        Dispatcher hiloUI;
+
+        ServidorJuegoService service = new();
 
         private Vista _vistaActual = Vista.AbrirSala;
+        private string? nombreServidor;
 
         public Vista VistaActual
         {
             get => _vistaActual;
             set { _vistaActual = value; OnPropertyChanged(); }
+        }
+        public string? NombreServidor { get => nombreServidor; set { nombreServidor = value; OnPropertyChanged(); } }
+        public string? NombreCliente { get; set; }
+
+        public string Mensaje { get; set; } = "";
+
+        public ICommand AbrirSalaCommand { get; set; }
+        public ICommand ElegirPokemonCommand { get; set; }
+
+        public ServidorViewmodel()
+        {
+            hiloUI = Dispatcher.CurrentDispatcher;
+            AbrirSalaCommand = new RelayCommand(AbrirSala);
+            service.ClienteConectado += Service_ClienteConectado;
+            service.PartidaIniciada += Service_PartidaIniciada;
+        }
+
+        private void Service_PartidaIniciada()
+        {
+            hiloUI.Invoke(() =>
+            {
+                VistaActual = Vista.Juego;
+            });
+        }
+
+        private void Service_ClienteConectado(string obj)
+        {
+            hiloUI.Invoke(() =>
+            {
+                NombreCliente = obj;
+                Mensaje = "Se unio el jugador: " + obj;
+                VistaActual = Vista.ElegirPersonaje;
+
+                OnPropertyChanged(nameof(Mensaje));
+            });
+            
+            
+        }
+
+        private void AbrirSala()
+        {
+            if(!string.IsNullOrWhiteSpace(NombreServidor) && NombreServidor.Length >= 3)
+            {
+                service.AbrirSala(NombreServidor);
+                VistaActual= Vista.EsperandoJugador;
+            }
+            else
+            {
+                Mensaje = "Escoja un nombre de al menos 3 caracteres";
+                OnPropertyChanged(nameof(Mensaje));
+            }
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
--- AdivinaQuienCliente/Viewmodels/ClienteViewmodel.cs
+++ AdivinaQuienCliente/Viewmodels/ClienteViewmodel.cs
@@ -1,11 +1,17 @@
 ﻿using AdivinaQuienCliente.Models;
+using AdivinaQuienCliente.Services;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Net;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Windows.Input;
 using System.Windows.Threading;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AdivinaQuienCliente.Viewmodels
 {
@@ -21,7 +27,6 @@ namespace AdivinaQuienCliente.Viewmodels
 
     public class ClienteViewmodel : INotifyPropertyChanged
     {
-
         public ObservableCollection<Pokemon> Pokemons { get; set; } = new ObservableCollection<Pokemon>()
         {
             new Pokemon { Nombre = "Alakazam" },
@@ -52,12 +57,78 @@ namespace AdivinaQuienCliente.Viewmodels
 
         Dispatcher dispatcher;
 
+        ClienteJuegoService service = new();
+
+        public string? NombreCliente { get; set; }
+        public string? NombreServidor { get; set; }
+        public string? DireccionIP { get; set; } = "127.0.0.1";
+        public string? Mensaje { get; set; } = "";
+
         private Vista _vistaActual = Vista.UnirseSala;
 
         public Vista VistaActual
         {
             get => _vistaActual;
             set { _vistaActual = value; OnPropertyChanged(); }
+        }
+
+        public ICommand UnirseSalaCommand { get; set; }
+        public ClienteViewmodel()
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+            UnirseSalaCommand = new RelayCommand(UnirseSala);
+
+            service.ClienteAceptado += Service_ClienteAceptado;
+            service.ClienteRechazado += Service_ClienteRechazado;
+            service.ServerEscogePokemon += Service_ServerEscogePokemon;
+            
+        }
+
+        private void Service_ServerEscogePokemon()
+        {
+            Mensaje = "";
+            OnPropertyChanged(Mensaje);
+            VistaActual= Vista.ElegirPersonaje;
+        }
+
+        private void Service_ClienteRechazado()
+        {
+            dispatcher.BeginInvoke(() =>
+            {
+                Mensaje = "El nombre seleccionado ya esta siendo utilizado";
+                OnPropertyChanged(nameof(Mensaje));
+            });
+        }
+
+        private void Service_ClienteAceptado()
+        {
+            dispatcher.BeginInvoke(() =>
+            {
+                NombreServidor = service.Servidor.Nombre;
+                VistaActual = Vista.EsperandoJugador;
+                OnPropertyChanged(nameof(NombreServidor));
+                Mensaje = $"Espera mientras {NombreServidor} escoje su pokemon";
+                OnPropertyChanged(nameof(Mensaje));
+            });
+        }
+
+        private void UnirseSala()
+        {
+            if (string.IsNullOrWhiteSpace(NombreCliente) || NombreCliente.Length<3)
+            {
+                Mensaje = "Elija un nombre de al menos tres caracteres";
+                OnPropertyChanged(nameof(Mensaje));
+                return;
+            }
+
+            if (!IPAddress.TryParse(DireccionIP, out IPAddress ip))
+            {
+                Mensaje = "Escriba una direccion ip correcta";
+                OnPropertyChanged(nameof(Mensaje));
+                return;
+            }
+
+            service.Conectar(ip, NombreCliente);
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
